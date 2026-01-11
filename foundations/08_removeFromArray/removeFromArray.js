8a2e4cb630ac93dd79c757e81a489f103dd8cd58
JustAGuy <jehannabar68@gmail.com>
2026-01-11 22:00:50 +0800

    Completed reverse, remove, and sum

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    for (const arg of theArgs) {
+        array = array.filter( arr => arr !== arg)
+    }
+
+    return array;
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
 
+    if (num1 > num2) {
+        bigger = num1;
+        smaller = num2;
+    }
+    else {
+        bigger = num2;
+        smaller = num1;
+    }
+
+    let answer = 0;
+    for (let i = smaller; i <= bigger; i++) {
+        answer += i;
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
