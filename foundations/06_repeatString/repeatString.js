16050b4d3474190f29f7b8130dbf3c44af76cfa9
JustAGuy <jehannabar68@gmail.com>
2026-01-11 21:17:58 +0800

    Solved repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(text = '', num = 0) {
+    let message = text;
 
+    if (num < 0)
+        return 'ERROR';
+
+    if (num === 0)
+        return "";
+
+    for (let i = 1; i < num; i++) {
+        text = text.concat(message);
+    }
+
+    return text;
 };
 
 // Do not edit below this line
